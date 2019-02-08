d574482476b7fc073a06f2cc4cee4b75ffb683e5
Navis <michael.bearly@hotmail.com>
2019-02-08 18:01:51 +0530

    adding home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,98 +1,56 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Strings Unlimited | a complete apparel solution | Chennai | India</title>
+   <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}" />
+</head>
+<body>
+    <div class="wrap">
+        <div id="header">
+            <div id="logo">
+                <img src="{{ URL::asset('images/logo.bmp') }}" alt="Strings Unlimited" />
+                <div>
+                    a complete apparel solution . . .</div>
             </div>
+            <ul id="nav">
+                <li><a href="index.html">Home</a></li>
+                <li><a href="contactus.html">Contact Us</a></li>
+            </ul>
         </div>
-    </body>
+        
+        
+        <div id="Ads">
+            <marquee scrollamount="4" onmouseover="this.stop();" onmouseout="this.start();"><a href="contactus.html"> Strings Unlimited is coming up with an exciting offer... Hurry!!!</a></marquee>
+        </div>
+        <!-- /header -->
+        <div id="content">
+            <table id="Mainpage">
+                <tr>
+                    <td style="width: 100%;">
+                        <div id="lefttd">
+                            <object width="1000" height="250" title="Strings Unlimited">
+                                <param name="movie" value="{{ URL::asset('images/movie2.swf') }}" />
+                                <param name="quality" value="high" />
+                                <param name="wmode" value="transparent" />
+                                <embed src="{{ URL::asset('images/movie2.swf') }}" width="1280" height="330" wmode="transparent"></embed>
+                            </object>
+                        </div>
+                    </td>
+                </tr>
+            </table>
+            <!-- /content -->
+        </div>
+        <div class="clearfix">
+            <p>
+                <b style="color: White; font-size: 16px;">Welcome to Strings Unlimited</b></p>
+            Strings Unlimited is a professionally managed progressive manufacturing/sourcing company engaged
+            in apparel merchandise classifications from Asia.<br />
+            Our presence in India and UK enables us to provide end to end services for a wide
+            gamut of products to our international clients.<br />
+            Our exhaustive range of products are garments for Men, Women, Kids, Beach wear,
+            Formal Wear, Sleep Wear/ Undergarments, Sport Wear and Home Furnishings.<br />
+        </div>
+    </div>
+</body>
 </html>
